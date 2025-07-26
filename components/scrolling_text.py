a6b75263fb2efcf3857fbdd4d429ec61f18885e2
duyanhdangquao <nhanthanbich69@gmail.com>
2025-07-26 10:37:33 +0700

    Update scrolling_text.py

--- components/scrolling_text.py
+++ components/scrolling_text.py
@@ -1,7 +1,7 @@
 def scrolling_message(text):
     import streamlit as st
     st.markdown(f"""
-    <marquee behavior='scroll' direction='left' scrollamount='25' style='color:#FFEB3B; font-size:30px; font-weight:bold; background-color:black; padding:10px;'>
+    <marquee behavior='scroll' direction='left' scrollamount='20' style='color:#FFEB3B; font-size:30px; font-weight:bold; background-color:black; padding:10px;'>
         {text}
     </marquee>
     """, unsafe_allow_html=True)
